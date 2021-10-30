972d8d98a7fb2fd40671d717be4217ac7e03b2fd
Annie <75851579+annie910201@users.noreply.github.com>
2021-10-31 00:55:23 +0800

    Add files via upload

---
+++ Velocity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickAndRestriction
+{
+    class Velocity
+    {
+        int v;
+        public int getV(Color color)
+        {
+            if (color == Color.Blue) v = 10;
+            else if (color == Color.Red) v = 20;
+            else if (color == Color.Green) v = 30;
+            return v;
+        }
+    }
+}
---
+++ shot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickAndRestriction
+{
+    class shot:Velocity
+    {
+        int shotV;
+        public new int getV(Color color)
+        {
+            if (color == Color.Blue) shotV = 10;
+            else if (color == Color.Red) shotV = 20;
+            else if (color == Color.Green) shotV = 30;
+            return shotV;
+        }
+    }
+}
